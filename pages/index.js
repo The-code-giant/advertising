d1f9b7751fdfa21957ff1adb23d1223662b61eec
Ali  Alizada <akbar.alizadeh5@gmail.com>
2021-06-16 02:02:44 -0700

    add images

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,104 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import styles from '../styles/Home.module.scss'
+import { useState } from 'react'
 export default function Home() {
+  const [step, setStep] = useState(1)
+  const [data, setData] = useState({})
+  const changeStep = (newStep, data) => {
+    setData(CONTENT[data])
+    setStep(newStep)
+  }
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className={styles.page}>
+        {step === 1 && (
+          <StepOne changeStep={changeStep} />
+        )}
+        {step === 2 && (
+          <StepTwo changeStep={changeStep} data={data} />
+        )}
+        {step === 3 && (
+          <StepThree changeStep={changeStep} />
+        )}
+      </div>
     </div>
   )
+}
+const StepTwo = ({ data, changeStep }) => {
+  return (
+    <>
+      <div className={styles.logoWrapper}>
+        <img src="/images/logo.png" />
+      </div>
+      <h2 style={{ textAlign: 'center' }}>{data.title}</h2>
+      <div>
+        <ul>
+          { 
+            data.children.map(child => {
+              return <li>{child.text}</li>
+            })
+          }
+          <li>dad</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+const StepThree = () => {
+  return (
+    <div>Three</div>
+  )
+}
+const StepOne = ({ changeStep }) => {
+  return (
+    <>
+      <div className={styles.logoWrapper}>
+        <img src="/images/logo.png" />
+      </div>
+      <h2 style={{ textAlign: 'center' }}>DE QUEL DESSERT GLACÉ AVEZ-VOUS ENVIE CET ÉTÉ?</h2>
+      <div className={styles.homeList}>
+        <ul>
+          <li onClick={() => changeStep(2, 'fabulous')}>Les Fabuleux<img src="/images/lesFabuleux.png" /></li>
+          <li onClick={() => changeStep(2, 'classic')}>Les Classiques<img src="/images/classics.png" /></li>
+          <li onClick={() => changeStep(2, 'boire')}>À boire<img src="/images/drink.png" /></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+const CONTENT = {
+  classic: {
+    title: "Découvrez tous nos classiques!",
+    backgroundUrl: '/images/icecreems.png',
+    children: [
+      { text: "Crème Glacée Molle", title: "Découvrez nos crèmes molles enrobées!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+      { text: "Crème Glacée Molle Enrobée", title: "Découvrez notre Banane royale!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+      { text: "Découvrez notre Brownie!", title: "Découvrez notre Brownie!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+      { text: "Vic-Mixte", title: "Découvrez notre Poutine Glacée!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+    ]
+  },
+  fabulous: {
+    title: "Découvrez tous nos fabuleux!",
+    backgroundUrl: '/images/icecreems2.png',
+    children: [
+      { text: "Poutine glacée", title: "Découvrez nos crèmes molles!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+      { text: "Brownie", title: "Découvrez nos slush!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+      { text: "Banane Royale", title: "Découvrez nos givrés!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+    ]
+  },
+  boire: {
+    title: "Découvrez tous nos fabuleux!",
+    backgroundUrl: '/images/icecreems2.png',
+    children: [
+      { text: "Givré", title: "Découvrez nos Vic-Mixtes!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+      { text: "Slush", title: "Découvrez nos sundaes!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+      { text: "Milkshake", title: "Découvrez nos Milkshakes!", backgroundUrl: '/images/icecreems.png', link: { text: "Trouver une succursale", href: "https://google.com" } },
+    ]
+  }
 }
